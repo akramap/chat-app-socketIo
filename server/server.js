5c911aabf858c2382a01b6a45d9b33f10434a086
akram <akram@cartoonmango.com>
2019-04-27 19:15:45 +0530

    connect and disconnect socket event handler

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
 // import is a ES6 feature not suppoeted by nodejs by default and it can  supported by babel right now 
 import path from "path";
+// express internally uses inbuilt module called http to create server so import. 
+import http from "http";
+
 import express from "express";
+// socketIo
+import socketIo from "socket.io";
+
 
 // this connects server to the client,setting up the path i.e currentdir/public.
 const publicPath=path.join(__dirname,'../public');
@@ -11,10 +17,24 @@ const port = process.env.PORT || 3000;
 
 // configure express application.
 const app = express();
+// express works with http.
+const server = http.createServer(app);
+
+const io=socketIo(server);
+
 
 // using the express middleware
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+// to establish socket connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+  // to disconnect socket
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    });
+  });
+
+server.listen(port,()=>{
     console.log(`Server is up on ${port}`);
 });
